242435794c4b419e9519fda1a8e4098473fa3f08
sjgraffjr <sjgraffjr@gmail.com>
2017-10-03 18:56:41 -0500

    showed the icon if the user gets it right or wrong

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,49 @@
-let correctAnswers = 0
-let inCorrectAnswers = 0
+let questionsAnswered = 0
 const checkComplete = ()=> {
 
-	return correctAnswers + inCorrectAnswers === $('[data-correct-answer]').length
+	return questionsAnswered === $('[data-correct-answer]').length
 
 }
 
 const checkAnswer = (e)=> {
-	const $el = $($('[data-correct-answer]')[correctAnswers + inCorrectAnswers])
+	
+	const $el = $($('[data-correct-answer]')[questionsAnswered])
 	const correctAnswer = $el.data('correctAnswer')
 	const selectedAnswer = $(e.target).data('possibleAnswer')
+	$el.data('userAnswer', selectedAnswer)
+	const $correctIcon = $('[data-correct]').hide()
+	const $incorrectIcon = $('[data-incorrect]').hide()
 	if (correctAnswer === selectedAnswer){
-		correctAnswers += 1
-		// alert("Well done, Lad!")
-		const $greenCheck = $('<img src ="http://www.thomasclausen.net/wp-content/plugins/wp-support-plus-responsive-ticket-system/asset/images/checkmark.png" alt="green check logo">').css({ width: '50px', height: '50px' });
-		$('.greencheck_location').append($greenCheck);
+		$correctIcon.show()
 	}else{
-		inCorrectAnswers += 1
-		const $redX = $('<img src="https://cdn.pixabay.com/photo/2012/04/12/20/12/x-30465_960_720.png">').css({ width: '50px', height: '50px' });
-		$('.redx_location').append($redX);
+		$incorrectIcon.show()
 	}
-	$el.hide()
+	
+}
 
-	const nextElement = $($('[data-correct-answer]')[correctAnswers + inCorrectAnswers])
+const correctAnswers = () =>{
+ const $els = $('[data-correct-answer]')
+ let count = 0
+ for(let i = 0; i < $els.length; i++ ){
+ 	let $el = $($els[i]) 
+ 	if($el.data('correctAnswer') === $el.data('userAnswer')){
+ 		count += 1
+ 	}
+ }
+ return count
+}
+
+const nextQuestion = () =>{
+	questionsAnswered += 1
+	const $el = $($('[data-correct-answer]')[questionsAnswered - 1])
+	$el.hide()
+	$('[data-correct]').hide()
+	$('[data-incorrect]').hide()
+	const nextElement = $($('[data-correct-answer]')[questionsAnswered])
 	nextElement.show()
 	if(checkComplete()){
-		// alert(`You got ${correctAnswers} out of ${correctAnswers + inCorrectAnswers}`)
-		const $p = $('<p class="final_score">').text(`You got ${correctAnswers} out of ${correctAnswers + inCorrectAnswers}`)
+		
+		const $p = $('<p class="final_score">').text(`You got ${correctAnswers()} out of ${questionsAnswered}`)
 		$(".section").append($p)
 		$("button").hide()
 		//instead of alerting build a paragraph elemnt with jquery and add this as the text of it and append it to the page
@@ -34,4 +51,5 @@ const checkAnswer = (e)=> {
 	}	
 }
 $('[data-possible-answer]').on('click',checkAnswer)
+$('[data-next]').on('click',nextQuestion)
 
